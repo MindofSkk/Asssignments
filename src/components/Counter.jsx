d6c4ed2d0a1ce118872d4325a87db4c989709d6b
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-05-18 00:32:45 +0530

    react

---
+++ src/components/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export const Counter = () => {
+    // let count=0;
+    const[count,setCount]=useState(0)
+    function inr()
+    {
+  setCount(count+1)
+    }
+    function der()
+    {
+        setCount(count-1)
+
+
+    }
+  return (
+    <>
+    
+    <div>Counter: {count}
+    
+    </div>
+    <div>
+        <button onClick={inr}>incrment</button>
+        <button onClick={der}>decrement</button>
+    </div>
+    
+    </>
+
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import css from "./Navbar.css";
+
+export const Navbar = () => {
+  let value={textDecoration:"none"};
+  return (
+    <div>
+      <div className="main">
+        <div id="logo">LOGOBAKERY</div>
+
+        <ul>
+        <a href="" style={value}><li>Service</li></a>  
+        <a href="" style={value}><li>projects</li></a>  
+         <a href="" style={value} ><li>About</li></a> 
+        </ul>
+        <div>
+          <button id="but">contact</button>
+        </div>
+      </div>
+    </div>
+  );
+};
