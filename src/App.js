25e91aa18b6c483c102aa291501c671447c8f343
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-05-25 22:38:53 +0530

    Sachin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar } from './components/Navbar/Navbar';
-import { Counter } from './components/Counter/Counter';
-import { Menu } from './components/Menu/Menu';
-import { Todo } from './components/Todo/Todo';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Counter } from "./components/Counter/Counter";
+import { Menu } from "./components/Menu/Menu";
+import { Todo } from "./components/Todo/Todo";
 function App() {
   return (
     <>
-    {/* <Navbar/>
-    <Counter/>
-   <Menu /> */}
-   <Todo/>
-
-    
+      {/* <Navbar/>
+        <Counter/>
+        <Menu /> */}
+      {/* <Todo /> */}
     </>
   );
 }
