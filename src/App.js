05c799445f9012ae6629a6b29997bf2684296f04
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-05-25 23:48:51 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Counter } from "./components/Counter/Counter";
 import { Menu } from "./components/Menu/Menu";
 import { Todo } from "./components/Todo/Todo";
+import { Paymentcard } from "./components/PaymentCard/Paymentcard";
+import payment from "./data/payment.json"
 function App() {
+
   return (
     <>
       {/* <Navbar/>
         <Counter/>
         <Menu /> */}
       {/* <Todo /> */}
+      <Paymentcard payments={payment} />
     </>
   );
 }
---
+++ src/components/PaymentCard/Paymentcard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Paymentcard.css";
+
+export const Paymentcard = (props) => {
+  //   let bcolor = props.backgroundcolor;
+  // console.log(props.Payments.date)
+  // console.log(props.payments[0].date)
+
+  return (
+    <>
+      {props.payments.map((e, index) =>   (
+         
+        <div className="box" style={{ background: e.backgroundcolor }}>
+          <div className="date">
+            <h3>{e.date}</h3>
+
+            <img src={e.logo} alt="" />
+          </div>
+
+          <div className="but">
+            <button>Case Study</button>
+          </div>
+
+          <div id="name">
+            <h1>{e.heading}</h1>
+          </div>
+
+          <div className="ls">
+            <h3>
+              {e.subheading}-{e.devices}
+            </h3>
+
+            <i className="fas fa-arrow-right"></i>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
