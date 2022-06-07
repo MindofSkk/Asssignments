58abedac659b3f48b619450eab4a16f9239a031e
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-06-07 20:38:26 +0530

    side bar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import SimpleSidebar from "./components/Chakra-sidebar/Sidebar";
 // import { Navbar } from "./components/Navbar/Navbar";
 // import { Counter } from "./components/Counter/Counter";
 // import { Menu } from "./components/Menu/Menu";
@@ -13,10 +14,9 @@ import "./App.css";
 // import { Reducercounter } from "./components/useReducer/Reducercounter";
 // import { Home } from "./components/Routing-lec/Home";
 // import TodoList from "./components/useEffect/TodoList";
-import TodoApp from "./components/todousereducer/TodoApp";
-import { useState } from "react";
+// import { useState } from "react";
 function App() {
-  const[ShowCount,setShowCount]=useState(true)
+  // const[ShowCount,setShowCount]=useState(true)
 
   return (
     <>
@@ -32,10 +32,11 @@ function App() {
       
       {/* <Reducercounter/> */}
       {/* <SimpleSidebar/> */}
+      <SimpleSidebar/>
       {/* <Home/> */}
       {/* first start the json server & then use(useReducer)then use todo */}
       {/* <TodoList/>  */}
-      <TodoApp/>
+      {/* <TodoApp/> */}
      
     </>
   );
